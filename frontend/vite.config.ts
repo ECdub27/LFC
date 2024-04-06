4dd292352a083ed79b6464eec771b77f68594571
Brody <brodyover@gmail.com>
2024-04-06 12:30:33 -0400

    fix structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api': {
-        target:'https://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        
-      }
-  }
-},
   plugins: [react()],
 })
